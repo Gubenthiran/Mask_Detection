1613c3bb9d8e284aeaa239374a8f4a075199cb42
Gubenthiran <717821i218@kce.ac.in>
2023-09-06 03:54:36 +0530

    gubi

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import numpy as np
+from keras.preprocessing import image
+import tensorflow as tf
+import tensorflow_hub as hub
+from PIL import Image  # Import the Image module from PIL
+
+# Define a custom object scope to register the KerasLayer
+custom_object_scope = tf.keras.utils.custom_object_scope({'KerasLayer': hub.KerasLayer})
+
+# Load your trained model within the custom object scope
+with custom_object_scope:
+    model = tf.keras.models.load_model('model.h5')
+
+# Streamlit app code
+st.title('Mask Detection App')
+
+# File uploader to upload an image for prediction
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg"])
+
+if uploaded_file is not None:
+    # Display the uploaded image
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+
+    # Preprocess the image for prediction
+    img = image.resize((64, 64))
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
+
+    # Make predictions
+    prediction = model.predict(img)
+    max_index = np.argmax(prediction)
+
+    # Define class labels
+    class_labels = ["With mask", "Without mask"]
+
+    # Display the prediction result
+    st.write(f"Prediction: {class_labels[max_index]} (Probability: {prediction[0][max_index]:.2f})")
